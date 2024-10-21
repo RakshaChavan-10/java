a8a528b72230e2c62afc3d061390ea650f4fd66b
RakshaChavan-10 <rakshachavan.xworkz@gmail.com>
2024-10-21 11:59:50 +0530

    new floder created

---
+++ CoolDrinks.java
@@ -0,0 +1,17 @@
+class CoolDrinks{
+     public static void main( String drinks[]){
+     String name = "Pepsi";
+     String quantity = "500ml";
+     String mfgDate = "12/10/23";
+     String expDate = "12/10/24";
+     int price  = 60;
+     System.out.println(" CoolDrinks name is    "+name);
+     System.out.println("CoolDrinks Quantity is "+quantity);
+     System.out.println("cooldrinks mfgDate is  "+mfgDate);
+     System.out.println("CoolDrinks expDate is  "+expDate);
+     System.out.println("CoolDrinks price is   "+price);
+                      }
+}
+
+
+
